8b847110ca90d86aac81df76ad5d35d1c947aff6
James Hansen <jameszenartist@gmail.com>
2023-05-15 03:37:34 -0700

    fixed Emailjs config

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,20 @@
 import styles from "../css/Contact.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export function Form() {
   interface FormValues {
-    name: string;
-    email: string;
+    from_name: string;
+    user_email: string;
     message: string;
   }
   const initialFormValues: FormValues = {
-    name: "",
-    email: "",
+    from_name: "",
+    user_email: "",
     message: "",
   };
 
+  const form = useRef<HTMLFormElement | null>(null);
   const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
 
   const handleInputChange = (
@@ -25,20 +27,40 @@ export function Form() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(formValues);
+    //Emailsjs function:
+    emailjs
+      .sendForm(
+        "VITE_SERVICE_ID",
+        "VITE_TEMPLATE_ID",
+        form.current!,
+        "VITE_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setFormValues({
+      from_name: "",
+      user_email: "",
+      message: "",
+    });
   };
 
   return (
     <>
-      <form method="POST" onSubmit={handleSubmit}>
-        <label htmlFor="name">
+      <form ref={form} onSubmit={handleSubmit}>
+        <label htmlFor="from_name">
           Name:
           <br />
         </label>
         <input
-          // id="name"
           type="text"
-          name="name"
-          value={formValues.name}
+          name="from_name"
+          value={formValues.from_name}
           onChange={handleInputChange}
           size={20}
           placeholder="Enter your name"
@@ -46,15 +68,14 @@ export function Form() {
           aria-required="true"
         />
         <br />
-        <label htmlFor="email">
+        <label htmlFor="user_email">
           Email:
           <br />
         </label>
         <input
-          type="text"
-          // id="email"
-          name="email"
-          value={formValues.email}
+          type="email"
+          name="user_email"
+          value={formValues.user_email}
           onChange={handleInputChange}
           size={20}
           placeholder="Enter your email"
@@ -74,7 +95,6 @@ export function Form() {
           <br />
         </label>
         <textarea
-          // id="message"
           name="message"
           value={formValues.message}
           onChange={handleInputChange}
