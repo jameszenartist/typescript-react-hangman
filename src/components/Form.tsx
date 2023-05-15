d6db40914b55fb7476e936284f3d4bfd472c6e3d
James Hansen <jameszenartist@gmail.com>
2023-05-15 02:09:43 -0700

    implemented Emailjs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import styles from "../css/Contact.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export function Form() {
   interface FormValues {
@@ -12,7 +13,7 @@ export function Form() {
     email: "",
     message: "",
   };
-
+  const form = useRef<HTMLFormElement | null>(null);
   const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
 
   const handleInputChange = (
@@ -25,19 +26,39 @@ export function Form() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(formValues);
+    //Emailsjs function:
+    emailjs
+      .sendForm(
+        "VITE_SERVICE_ID",
+        "VITE_TEMPLATE_ID",
+        form.current!,
+        "VITE_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setFormValues({
+      name: "",
+      email: "",
+      message: "",
+    });
   };
 
   return (
     <>
-      <form method="POST" onSubmit={handleSubmit}>
+      <form ref={form} method="POST" onSubmit={handleSubmit}>
         <label htmlFor="name">
           Name:
           <br />
         </label>
         <input
-          // id="name"
           type="text"
-          name="name"
+          name="from_name"
           value={formValues.name}
           onChange={handleInputChange}
           size={20}
@@ -51,9 +72,8 @@ export function Form() {
           <br />
         </label>
         <input
-          type="text"
-          // id="email"
-          name="email"
+          type="email"
+          name="user_email"
           value={formValues.email}
           onChange={handleInputChange}
           size={20}
@@ -74,7 +94,6 @@ export function Form() {
           <br />
         </label>
         <textarea
-          // id="message"
           name="message"
           value={formValues.message}
           onChange={handleInputChange}
