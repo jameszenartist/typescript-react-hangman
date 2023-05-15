02d25603a524fbe2b7945571731958664ee364a2
James Hansen <jameszenartist@gmail.com>
2023-05-15 05:37:40 -0700

    update emailjs config

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -32,7 +32,7 @@ export function Form() {
         "VITE_SERVICE_ID",
         "VITE_TEMPLATE_ID",
         form.current!,
-        "YOUR_PUBLIC_KEY"
+        "VITE_PUBLIC_KEY"
       )
       .then(
         (result) => {
